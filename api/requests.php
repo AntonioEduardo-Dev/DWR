bf3698f3715fa72274768e1ecfdd5d21e9cf8147
KelpGF <kelpgf@gmail.com>
2021-09-04 16:31:49 -0300

    campos obrigatorios

--- api/requests.php
+++ api/requests.php
@@ -12,24 +12,28 @@ $routes = [
     "GET" => [
         'listarInsumos' => [
             "users" => ["user", "admin"],
+            "campos" => [],
             "class" => "Insumos"
         ],
     ],
     "POST" => [
         'cadastarInsumos' => [
             "users" => ["admin"],
+            "campos" => ["nome"],
             "class" => "Insumos"
         ],
     ],
     "PUT" => [
         'editarInsumos' => [
             "users" => ["admin"],
+            "campos" => ["nome"],
             "class" => "Insumos"
         ],
     ],
     "DELETE" => [
         'deletarInsumos' => [
             "users" => ["admin"],
+            "campos" => ["id"],
             "class" => "Insumos"
         ],
     ],
@@ -46,6 +50,22 @@ if (key_exists($funcionalidade, $metodos_disponiveis)) {
     $dados_funcao = $metodos_disponiveis[$funcionalidade];
 
     if (in_array($_SESSION['DWR_typeUser'], $dados_funcao['users'])) {
+        $campos_obrigatorios = $dados_funcao['campos'];
+
+        if (count($campos_obrigatorios) > 0) {
+            $campos_invalidos = false;
+
+            foreach ($campos_obrigatorios as $campo) {
+                $validacao_campo = dadosIsset($campo, $metodo);
+
+                if (empty($validacao_campo)) {
+                    $campos_invalidos = true;
+                    break;
+                }
+            }
+
+            if ($campos_invalidos) responseJson("warning", "Preencha os campos obrigatórios");
+        }
 
         if ($dados_funcao['class'] == 'Insumos') {
             require "models/Insumos.class.php";
