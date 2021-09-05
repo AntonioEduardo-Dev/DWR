e0645d9df878c857959f8bbe433985c09dae0ec2
KelpGF <kelpgf@gmail.com>
2021-09-05 12:59:14 -0300

    ajuste e pedidos

---
+++ api/controllers/ControllerPedidos.php
@@ -0,0 +1,85 @@
+<?php
+
+require "models/Pedidos.class.php";
+
+class ControllerPedidos {
+
+    private $funcao;
+    private $Pedidos;
+
+    private $type = "error";
+    private $message = "Não foi possível concluir a funcionalidade";
+    private $dataResponse = [];
+
+    public function __construct($funcao, $data) {
+        $this->funcao = $funcao;
+
+        if ($funcao == "cadastrarPedido" && is_array($data['insumos'] = json_decode($data['insumos']))) {
+            $insumos = "";
+
+            foreach ($data['insumos'] as $idx => $insumo) {
+                if ($idx > 0) $insumos .= ";";
+
+                $insumos .= "{$insumo->qtd}x {$insumo->nome}";
+                // $insumo .= "{$insumo['qtd']}x {$insumo['nome']}";
+            }
+
+            $data['insumos'] = $insumos;
+        }
+
+        $this->Pedidos = new Pedidos($data);
+    }
+
+    /*======================================================================================*/
+
+    private function cadastrarPedido() {
+        if ($this->Pedidos->cadastrarPedido()) {
+            $this->type = "success";
+            $this->message = "Pedido Inserido";
+        }
+
+        $this->retorno();
+    }
+
+    /*======================================================================================*/
+
+    private function listarPedidos() {
+        $this->dataResponse = $this->Pedidos->listarPedidos();
+
+        if (empty($this->dataResponse)) {
+            $this->type = "warning";
+            $this->message = "Nenhum Pedido Encontrado";
+        } else {
+            $this->type = "success";
+            $this->message = "Pedidos Encontrados";
+        }
+
+        $this->retorno();
+    }
+
+    /*======================================================================================*/
+
+    private function editarStatusPedido() {
+        if ($this->Pedidos->editarStatusPedido()) {
+            $this->type = "success";
+            $this->message = "Status do Pedido Atualizado";
+        }
+
+        $this->retorno();
+    }
+
+    /*======================================================================================*/
+
+    public function executarFuncao() {
+        $funcao = $this->funcao;
+        
+        return $this->$funcao();
+    }
+
+    /*======================================================================================*/
+
+    public function retorno() {
+        responseJson($this->type, $this->message, $this->dataResponse);
+    }
+
+}
--- api/models/Conexao.class.php
+++ api/models/Conexao.class.php
@@ -3,7 +3,6 @@
     class Conexao {
 
         function conectar(){
-
             $host= "mysql:host=localhost;dbname=dwr";
             $user= "root";
             $pass= "";
@@ -16,7 +15,54 @@
             } catch (Exception $e) {
                 echo "Erro: " . $e->getMessage();
             }
+        }
+        
+        /*======================================================================================*/
 
+        function getAll($table, $id = "") {
+            $connection = $this->conectar();
+    
+            try {
+                $sql = "SELECT * FROM {$table}";
+                (!empty($id)) && $sql .= " WHERE id = :id";
+
+                $consulta = $connection->prepare($sql);
+
+                (!empty($id)) && $consulta->bindParam(":id", $id);
+    
+                $consulta->execute();
+    
+                if ($consulta->rowCount() > 0) {
+                    return (!empty($id)) ? $consulta->fetch($connection::FETCH_ASSOC) : $consulta->fetchAll($connection::FETCH_ASSOC);
+                } else return [];
+                
+            } catch (PDOException $e) {
+                echo "Erro de autenticação: " . $e->getMessage();
+            } catch (Exception $e) {
+                echo "Erro: " . $e->getMessage();
+            }
+        }
+
+        /*======================================================================================*/
+        
+        function delete($id, $table) {
+            $connection = $this->conectar();
+    
+            try {
+                $sql = "DELETE FROM {$table} WHERE id = :id";
+    
+                $consulta = $connection->prepare($sql);
+
+                $consulta->bindParam(":id", $id);
+    
+                $consulta->execute();
+    
+                return ($consulta->rowCount() > 0) ? true : false;
+            } catch (PDOException $e) {
+                echo "Erro de autenticação: " . $e->getMessage();
+            } catch (Exception $e) {
+                echo "Erro: " . $e->getMessage();
+            }
         }
     }
 ?>
