474e771a7391785ffb03e326a452234c50d65dee
Antonio Eduardo <56364149+AntonioEduardo-sm@users.noreply.github.com>
2021-09-06 16:48:52 -0300

    imagem

---
+++ api/controllers/ControllerImagem.php
@@ -0,0 +1,34 @@
+<?php
+
+if(isset($_FILES['imagemInsumoUpload'])){
+    if (!empty($_FILES['imagemInsumoUpload'])){
+
+        $ext = strtolower(substr($_FILES['imagemInsumoUpload']['name'], -4)); // pegar extensão
+        
+        $extValidas = array(".jpg",".jpeg",".png"); // extensões validas
+        
+        $verifica = 0;
+        for ($i=0; $i < count($extValidas); $i++) { 
+            if ($ext == $extValidas[$i]) $verifica++;
+        }
+
+        if ($verifica > 0) {
+            $novoNome = md5(time()).$ext; // definir novo nome
+            $dir = "../../client/dist/img/insumos/"; // definir diretório para upload da imagem
+
+            // upload imagem
+            if (move_uploaded_file($_FILES['imagemInsumoUpload']['tmp_name'], $dir.$novoNome)) {
+                echo 'true-|-'.$novoNome;
+            }else{
+                echo 'false-|-Imagem_não_enviada!';
+            }
+        }else{
+            echo 'false-|-Extensão_Invalida!';
+        }
+
+    }else{
+        echo 'false-|-Erro_inesperado!';
+    }
+}
+
+?>
--- client/assets/js/secretario/insumos/cadastrar.js
+++ client/assets/js/secretario/insumos/cadastrar.js
@@ -1,12 +1,14 @@
 function cadastrar()
 {
+    nomeImagem = inserirImagem();
+
     var dados = {
         funcionalidade : "cadastarInsumo",
         nome : $("#nome_insumo").val(),
         categoria : $("#categoria_insumo").val(),
         disponibilidade : $('#disponibilidade_insumo').is(':checked') ? 'sim' : 'nao',
         descricao : $("#descricao_insumo").val(),
-        img : $("#imagem_insumo").val(),
+        img : nomeImagem,
     };
 
     var i = $(`#icone_cadastrar`);
@@ -44,6 +46,48 @@ function cadastrar()
 
             listar();
         }
+    });
+}
+
+function inserirImagem() 
+{
+    if($('#imagem_insumo')[0].files[0]) {
+        var data = new FormData();
+        data.append('imagemInsumoUpload', $('#imagem_insumo')[0].files[0]);
+        
+        $.ajax({
+            url: '../../../api/controllers/ControllerImagem.php',
+            data: data,
+            processData: false,
+            contentType: false,
+            type: 'POST',
+            success: function(data) 
+            {
+                var resultado = data.split('-|-');
+                if (resultado[0] == "true") {
+                    prod_imagem = resultado[1];
+                }else {
+                    prod_imagem = "imagem_indefinida.jpg";
+                }
+            }
+        });
+    }else{
+        prod_imagem = "imagem_indefinida.jpg";
+    }
+}
+
+function apagarImagem(nomeImagem) 
+{
+    var dados = {
+        apagarImagem : nomeImagem
+    }
+    
+    $.ajax({
+        url: '../../../api/controllers/ControllerImagem.php',
+        type: 'POST',
+        data: dados,
+        headers
+    }).done(function( retorno ) {
     });
 }
 
